e3e25f0c26b454a786b9ab9fe1301c2699c8a68e
M VEEKSHAN GOUD <157639872+mveekshan1@users.noreply.github.com>
2026-02-01 22:09:04 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
-    },
-  },
   server: {
     host: "::",
     port: 8080,
